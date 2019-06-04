b74cf9568de1e46c10e8301d2853b3795794c626
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-06-04 21:30:38 +0000

    optimized

--- slice.py
+++ slice.py
@@ -1,7 +1,6 @@
 import sys
 import argparse
 import cv2
-print(cv2.__version__)
 
 def extractImages(pathIn, pathOut):
     count = 0
@@ -11,8 +10,9 @@ def extractImages(pathIn, pathOut):
     while success:
       vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
       success,image = vidcap.read()
-      print ("Frame %d Created!" % count)
       cv2.imwrite( pathOut + "%d.jpg" % count, image)     # save frame as JPEG file
       count = count + 1
 extractImages("Data/presenter.mp4", "img/presenter/Presenter")#presenter
 #extractImages("Data/crowd.mp4", "img/crowd/Crowd")#crowd
+print("Done!")
+exit()
